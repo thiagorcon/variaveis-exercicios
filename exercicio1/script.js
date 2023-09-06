86b9b1362a694866d5aeee759c5553e6b14018fe
Thiago Rosa <thiagorcon@hotmail.com>
2023-09-06 13:51:32 -0300

    envio exercicio1

---
+++ exercicio1/script.js
@@ -0,0 +1,11 @@
+const nome = prompt('Qual o seu nome?')
+let idade = Number(prompt('Qual a sua idade?'))
+console.log(typeof nome);
+console.log(typeof idade)
+// nada foi emptresso pro que não foi atribuido uma variavel
+
+console.log(nome)
+console.log(idade)
+//agora foi impresso por que foi atribuido nos valores junto ao comando prompt nas variaveis
+
+console.log(`olá ${nome} , voce tem ${idade} anos.`);
